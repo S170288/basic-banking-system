dcc3281f91e8f43fac344bf3a324e6e11040b721
mohanraju <89908334+S170288@users.noreply.github.com>
2022-07-20 11:00:34 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	$host = "localhost";
         $user = "root";
         $pass = "";
-        $db = "sparksbank_db";
+        $db = "bank_db";
         $charset = "utf8mb4";
 
         $conn = mysqli_connect($host, $user, $pass, $db);
